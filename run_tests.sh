5c128eef4d400d3b553f9f0bdb1f017a32b53f68
Artyom Goncharov <m4yers@gmail.com>
2015-12-02 22:04:09 +0100

    Update test runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./install.sh
-./test/driver/htr.py test ./test/ ./_test/ _install/helium/helium -t run-fail
+./test/driver/htr.py test ./test/ ./_test/ _install/helium/helium -t parse-fail run-fail run-pass
--- test/driver/htr.py
+++ test/driver/htr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from enum import Enum
 import subprocess
 import argparse
 import string
@@ -19,168 +20,100 @@ RUNTIME_ERROR = re.compile(
     "\s*EXIT\s+(?P<line>\d+)\,(?P<column>\d+)\s+(?P<code>\d{4,}).*")
 
 
-class TestInfo():
+class Runner():
 
-    def __init__(self, compiler, folder, filename):
-        self.compiler = compiler
-        self.folder = folder
+    def __init__(self, command, is_reversed):
+        self.command = command
+        self.is_reversed = is_reversed
+
+    def run(self):
+        self.code = 0
+        try:
+            output = subprocess.check_output(
+                self.command, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            self.code = e.returncode
+            self.output = e.output
+
+    def is_ok(self):
+        result = self.code == 0
+        if self.is_reversed:
+            result = not result
+        return result
+
+    def get_output(self):
+        return self.output
+
+
+class Test():
+
+    def __init__(self, test_folder, temp_folder, filename):
+        self.temp_folder = temp_folder
+        self.test_folder = test_folder
         self.filename = filename
-        self.flags = []
-        self.errors = []
+        self.fullname = os.path.join(test_folder, filename)
+        self.compile_flags = []
+        self.execute_flags = []
+        self.expectations = []
+        self.runners = []
 
-    def add_error(self, line, code):
-        self.errors.append({'line': line, 'code': code, 'marked': False})
+    def dig(self):
+        lc = 0
+        with (open(self.fullname, 'r')) as f:
+            for line in f:
+                lc = lc + 1
 
-    def mark_error(self, line, code):
-        for error in self.errors:
-            if error['line'] == line and error['code'] == code:
-                error['marked'] = True
+                f = FLAG_LINE.match(line)
+                if f:
+                    # TODO split
+                    self.add_compile_flag(f.group('flags'))
+
+                t = TEST_LINE.match(line)
+                if t:
+                    self.add_expectation(str(lc), t.group('code'))
+
+    def add_expectation(self, line, code):
+        self.expectations.append({'line': line, 'code': code, 'marked': False})
+
+    def mark_expectation(self, line, code):
+        for e in self.expectations:
+            if e['line'] == line and e['code'] == code:
+                e['fulfilled'] = True
                 return
 
-    def has_unmarked_erros(self):
-        return bool(len([e for e in self.errors if not e['marked']]))
+    def has_unfulfilled(self):
+        return bool(len([e for e in self.expectations if not e['fulfilled']]))
 
-    def add_flags(self, option):
-        self.flags.append(option)
+    def add_compile_flag(self, option):
+        self.compile_flags.append(option)
 
-    def get_compile_command(self):
-        return "{} {}/{} {}".format(
-            self.compiler,
-            self.folder,
-            self.filename,
-            " ".join(self.flags))
+    def get_compile_flags(self):
+        return "{} {}".format(
+            self.fullname,
+            " ".join(self.compile_flags))
+
+    def add_runner(self, runner):
+        self.runners.append(runner)
+
+    def run(self):
+        print "Running {:.<80}".format(self.fullname),
+
+        for r in self.runners:
+            r.run()
+            if not r.is_ok():
+                print 'Fail\n{}\nOutput:\n{}'.format(
+                    self,
+                    r.output)
+                return
+
+        print "OK"
 
     def __str__(self):
-        return "TestInfo {}/{}\nerrors:\n{}\ncompile-flags:\n{}".format(
-            self.folder,
-            self.filename,
-            "\n".join(str(d) for d in self.errors),
-            " ".join(self.flags))
-
-
-class SPIMRunTestInfo(TestInfo):
-
-    def __init__(self, compiler, spim, folder, filename):
-        TestInfo.__init__(self, compiler, folder, filename)
-        self.spim = spim
-
-    def get_run_command(self):
-        return "{} -f {}/{}".format(
-            self.spim,
-            self.folder,
-            self.filename)
-
-
-def dig_file(info):
-    lc = 0
-    with (open(os.path.join(info.folder, info.filename), 'r')) as f:
-        for line in f:
-            lc = lc + 1
-
-            f = FLAG_LINE.match(line)
-            if f:
-                info.add_flags(f.group('flags'))
-
-            t = TEST_LINE.match(line)
-            if t:
-                info.add_error(str(lc), t.group('code'))
-
-
-def test_parse_fail(env):
-    test_folder = os.path.join(env.test_folder, 'parse-fail')
-    for f in (f for f in os.listdir(test_folder) if os.path.isfile(os.path.join(test_folder, f))):
-        info = TestInfo(env.compiler, test_folder, f)
-        dig_file(info)
-
-        output = None
-        try:
-            print "Running {:.<80}".format(os.path.join(test_folder, f)),
-            output = subprocess.check_output(
-                info.get_compile_command(), shell=True)
-        except subprocess.CalledProcessError as e:
-            output = e.output
-
-        if not output:
-            print 'Fail'
-            continue
-
-        for line in output.splitlines():
-            m = COMPILER_ERROR.match(line)
-            if not m:
-                continue
-            line = m.group('line')
-            code = m.group('code')
-            info.mark_error(line, code)
-
-        if info.has_unmarked_erros():
-            print 'Fail\n{}'.format(info)
-            print 'Compile command:\n{}'.format(info.get_compile_command())
-            print 'Compiler output:\n{}'.format(output)
-        else:
-            print 'OK'
-
-
-def test_run_fail(env):
-    test_folder = os.path.join(env.test_folder, 'run-fail')
-    temp_folder = os.path.join(env.temp_folder, 'run-fail')
-
-    if not os.path.exists(temp_folder):
-        os.makedirs(temp_folder)
-
-    for f in os.listdir(test_folder):
-
-        fullname = os.path.join(test_folder, f)
-
-        if not os.path.isfile(fullname):
-            continue
-
-        outfile = "{}/{}".format(temp_folder, f.replace(".he", ".s"))
-
-        info = SPIMRunTestInfo(env.compiler, "spim", test_folder, f)
-        info.add_flags("-o{}".format(outfile))
-        dig_file(info)
-
-        output = None
-        print "Running {:.<80}".format(fullname),
-        try:
-            output = subprocess.check_output(
-                info.get_compile_command(), shell=True)
-        except subprocess.CalledProcessError as e:
-            output = e.output
-
-        if not output:
-            print 'Fail'
-            continue
-
-        output = None
-        error = None
-        try:
-            output = subprocess.check_output(
-                "spim -f {}".format(outfile), shell=True)
-        except subprocess.CalledProcessError as e:
-            error = e
-
-        # We test for failing here
-        if not error:
-            print "Fail"
-            continue
-
-        # Making sure every error mentioned in the snippet is met
-        for line in error.output.splitlines():
-            m = RUNTIME_ERROR.match(line)
-            if not m:
-                continue
-            line = m.group('line')
-            code = m.group('code')
-            info.mark_error(line, code)
-
-        if info.has_unmarked_erros():
-            print 'Fail\n{}'.format(info)
-            print 'Compile command:\n{}'.format(info.get_compile_command())
-            print 'Compiler output:\n{}'.format(output)
-            print 'Run ouput:\n{}'.format(error.output)
-        else:
-            print 'OK'
+        return "Test {}\ncompile-flags: {}\nexpectations:\n{}".format(
+            self.fullname,
+            self.compile_flags,
+            "\n".join(str(d) for d in self.expectations),
+            " ".join(self.compile_flags))
 
 #
 # Actions
@@ -189,10 +122,47 @@ def test_run_fail(env):
 
 def test(options):
     for target in options.targets:
-        if target == 'parse-fail':
-            test_parse_fail(options)
-        elif target == 'run-fail':
-            test_run_fail(options)
+
+        test_folder = os.path.join(options.test_folder, target)
+        temp_folder = os.path.join(options.temp_folder, target)
+
+        if not os.path.exists(temp_folder):
+            os.makedirs(temp_folder)
+
+        for f in os.listdir(test_folder):
+
+            source_fullname = os.path.join(test_folder, f)
+            output_fullname = os.path.join(temp_folder, f.replace(".he", ".s"))
+
+            test = Test(test_folder, temp_folder, f)
+            test.add_compile_flag("-o{}".format(output_fullname))
+            test.dig()
+
+            if target == 'parse-fail':
+                test.add_runner(Runner("{} {}".format(
+                    options.compiler,
+                    test.get_compile_flags()),
+                    False))
+
+            elif target == 'run-fail':
+                test.add_runner(
+                    Runner("{} {}".format(
+                        options.compiler,
+                        test.get_compile_flags()),
+                        False))
+                test.add_runner(
+                    Runner("spim -f {}".format(output_fullname), True))
+
+            elif target == 'run-pass':
+                test.add_runner(
+                    Runner("{} {}".format(
+                        options.compiler,
+                        test.get_compile_flags()),
+                        False))
+                test.add_runner(
+                    Runner("spim -f {}".format(output_fullname), False))
+
+            test.run()
 
 #
 # Menu
