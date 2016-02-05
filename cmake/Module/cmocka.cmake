002ea1df67aafa99a2ea61ddae89ad696f6a2c66
Artyom Goncharov <m4yers@gmail.com>
2016-02-05 12:59:27 +0100

    Test Remove direct dependency on cmocka

--- cmake/Module/cmocka.cmake
+++ cmake/Module/cmocka.cmake
@@ -1,15 +1,6 @@
-set (CMOCKA "/Users/m4yers/Development/SDK/cmoka/cmocka-1.0.1" CACHE PATH
-    "CMocka test framework path")
-
-list (APPEND CMAKE_MODULE_PATH ${CMOCKA}/cmake/Modules)
-
-add_subdirectory (${CMOCKA} ${CMAKE_BINARY_DIR}/cmocka)
-
-include_directories (SYSTEM ${CMOCKA}/include)
-
 function (add_cmocka_test target)
     add_executable(${target} ${ARGN})
-    target_link_libraries(${target} cmocka_shared)
+    target_link_libraries(${target} /usr/local/lib/libcmocka.a)
 
     add_test(${target} ${target})
 
